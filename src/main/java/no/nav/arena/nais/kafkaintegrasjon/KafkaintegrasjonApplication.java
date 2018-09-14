6f7e15bee387287937844e377052459e1aba462e
Andreas Strand <andreas.strand@nav.no>
2018-09-14 13:00:19 +0200

    Initial commit.. with code..

---
+++ src/main/java/no/nav/arena/nais/kafkaintegrasjon/KafkaintegrasjonApplication.java
@@ -0,0 +1,12 @@
+package no.nav.arena.nais.kafkaintegrasjon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaintegrasjonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaintegrasjonApplication.class, args);
+    }
+}
