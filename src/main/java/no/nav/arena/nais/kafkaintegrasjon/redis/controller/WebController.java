f5286bbabf71ec946d6247de0e125a300a94023d
Audun Persson <audun.persson@nav.no>
2018-09-18 14:38:06 +0200

    Redis og Prometheus eksempel

---
+++ src/main/java/no/nav/arena/nais/kafkaintegrasjon/redis/controller/WebController.java
@@ -0,0 +1,92 @@
+package no.nav.arena.nais.kafkaintegrasjon.redis.controller;
+
+import java.util.Map;
+
+import io.prometheus.client.Counter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import no.nav.arena.nais.kafkaintegrasjon.redis.model.Customer;
+import no.nav.arena.nais.kafkaintegrasjon.redis.repo.CustomerRepository;
+
+
+@RestController
+public class WebController {
+
+	@Autowired
+	private CustomerRepository customerRepository;
+
+	// Prometheus
+	private static final Counter httpRequestsTotal = Counter
+			.build("http_requests_total", "Total number of HTTP Requests")
+			.labelNames("path")
+			.register();
+
+	@RequestMapping("/save")
+	public String save() {
+		// save a single Customer
+		customerRepository.save(new Customer(1, "Jack", "Smith"));
+		customerRepository.save(new Customer(2, "Adam", "Johnson"));
+		customerRepository.save(new Customer(3, "Kim", "Smith"));
+		customerRepository.save(new Customer(4, "David", "Williams"));
+		customerRepository.save(new Customer(5, "Peter", "Davis"));
+
+		// Prometheus - tell opp antall requests
+		httpRequestsTotal.labels("/save").inc();
+
+		return "Done";
+	}
+
+	@RequestMapping("/findall")
+	public String findAll() {
+		String result = "";
+		Map<Long, Customer> customers = customerRepository.findAll();
+
+		for (Customer customer : customers.values()) {
+			result += customer.toString() + "<br>";
+		}
+
+		// Prometheus - tell opp antall requests
+		httpRequestsTotal.labels("/findall").inc();
+
+		return result;
+	}
+
+	@RequestMapping("/find")
+	public String findById(@RequestParam("id") Long id) {
+		String result = "";
+		result = customerRepository.find(id).toString();
+
+		// Prometheus - tell opp antall requests
+		httpRequestsTotal.labels("/find-id").inc();
+
+		return result;
+	}
+
+	@RequestMapping(value = "/uppercase")
+	public String postCustomer(@RequestParam("id") Long id) {
+		Customer customer = customerRepository.find(id);
+		customer.setFirstName(customer.getFirstName().toUpperCase());
+		customer.setLastName(customer.getLastName().toUpperCase());
+
+		customerRepository.update(customer);
+
+		// Prometheus - tell opp antall requests
+		httpRequestsTotal.labels("/uppercase-id").inc();
+
+		return "Done";
+	}
+
+	@RequestMapping("/delete")
+	public String deleteById(@RequestParam("id") Long id) {
+		customerRepository.delete(id);
+
+		// Prometheus - tell opp antall requests
+		httpRequestsTotal.labels("/delete-id").inc();
+
+		return "Done";
+	}
+
+}
