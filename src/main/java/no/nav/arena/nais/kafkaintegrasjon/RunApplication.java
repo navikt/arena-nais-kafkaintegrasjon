95258341a763cd9a51aedc670608434aafbf64d9
Sigbjørn Myhre <sigbjorn.myhre@nav.no>
2018-09-19 14:25:53 +0200

    Add Prometheus functionality to TestController

--- src/main/java/no/nav/arena/nais/kafkaintegrasjon/RunApplication.java
+++ src/main/java/no/nav/arena/nais/kafkaintegrasjon/RunApplication.java
@@ -1,16 +1,19 @@
 package no.nav.arena.nais.kafkaintegrasjon;
 
 import no.nav.arena.nais.kafkaintegrasjon.kafka.KafkaEmbeddedServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 
 @SpringBootApplication
 public class RunApplication {
 
+    @Autowired
+    public ServletRegistrationBean prometheusServlet;
 
     public static void main(String[] args) {
         KafkaEmbeddedServer.startEmbeddedKafka();
         SpringApplication.run(RunApplication.class, args);
     }
-
 }
